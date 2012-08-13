e802a0fbaff434c4631e04872f421e210d66af6c
Jari Voutilainen <jari.voutilainen@iki.fi>
2012-08-13 13:13:34 +0300

    Remove warnings and add logout after sending message

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-var exec = require('child_process').exec;
 var pty = require('pty.js');
 /*
  * GET home page.
@@ -8,25 +7,27 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
-exports.coffee = function(req, res){
+exports.coffee = function (req, res) {
     var term = pty.spawn('bash', [], {
-        name: 'xterm-color',
-        cols: 80,
-        rows: 30,
-        cwd: process.env.HOME,
-        ebv: process.env
+        name:'xterm-color',
+        cols:80,
+        rows:30,
+        cwd:process.env.HOME,
+        ebv:process.env
     });
 
-    term.on('data', function(data){
-       console.log(data);
+    term.on('data', function (data) {
+        console.log(data);
     });
 
-    function done(err, stdout, stderr){
-        //console.log(stdout);
-        res.json({success: true});
+    function done() {
+        res.json({success:true});
     }
 
     term.write('screen -x\r');
-    term.write('/msg Zharktas moi\r')
+    term.write('/msg Zharktas moi\r');
+    term.write('^a^d\r');
+    termi.write('logout\r');
+    done();
     console.log(term.process);
-}
+};
